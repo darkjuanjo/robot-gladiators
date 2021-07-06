84c19f022bb39b46f47d05e8dfc21bd91bec97b2
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-07-05 21:26:59 -0700

    fixed empty or null name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -155,8 +155,20 @@ switch (shopOptionPrompt) {
     return value;
   }
 
+  // function to set name
+  var getPlayerName = function() {
+  var name = "";
+      while(name === "" || name === null)
+      {
+        name = window.prompt("What is your robot's name?")
+      }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
